2205852bea636eb0544d66e53a435a3771c9df83
Tarnished <hanura423@gmail.com>
2025-11-09 10:28:59 +0700

    Update movie.controller.spec.ts

--- src/movie/movie.controller.spec.ts
+++ src/movie/movie.controller.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovieController } from './movie.controller';
+import { MovieService } from './movie.service';
 
 describe('MovieController', () => {
   let controller: MovieController;
+  let movieService: MovieService;
+
+  const mockMovieService = {
+    getMovies: jest.fn().mockResolvedValue('Movies retrieved successfully'),
+    createMovie: jest.fn().mockResolvedValue('Movie created successfully'),
+    updateMovie: jest.fn().mockResolvedValue('Movie updated successfully'),
+    deleteMovie: jest.fn().mockResolvedValue('Movie deleted successfully'),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MovieController],
+      providers: [
+        {
+          provide: 'MovieService',
+          useValue: mockMovieService,
+        },
+      ],
     }).compile();
 
     controller = module.get<MovieController>(MovieController);
+    movieService = module.get<MovieService>(MovieService);
   });
 
   it('Should return MovieResponse', () => {
